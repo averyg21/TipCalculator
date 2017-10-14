2869aec1c964ea2e3b5b721f6d98a79963fed245
AveryGrimes <averygrimes685@yahoo.com>
2017-10-14 14:42:32 -0700

    Added import statement & currency/percent formatters

--- app/src/main/java/com/example/averygrimes/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/averygrimes/tipcalculator/MainActivity.java
@@ -2,8 +2,22 @@ package com.example.averygrimes.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final NumberFormat currencyFormat =
+            NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat =
+            NumberFormat.getPercentInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
